071ac92b7cd6c52ee4384a028480560f7cafd1e1
kensuke-y <166479001+kensuke-y@users.noreply.github.com>
2024-05-24 12:40:10 +0900

    連想配列を作って値とキーを出力しよう

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<<!DOCYYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $personal_data=["name" => "onion","price" => 200,"weight"=>"160"];
+
+        print_r($personal_data);
+        ?>
+    </p>
+</body>
+
+</html>
